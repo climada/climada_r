d584c138b6b15a397148aef5bf96ba7a706308fe
climada <support@sms2c.com>
2020-10-08 14:47:10 +0800

    Update 2020 China Yangtze Flooding.R

--- Examples/2020 China Yangtze Flooding.R
+++ Examples/2020 China Yangtze Flooding.R
@@ -1,31 +1,28 @@
 # options(scipen=999)
+#Charts properties
+DP_Pointsize <- 20
+DP_Height <- 600
+DP_Width <- 800
+DP_Save <- T
+
+
 # Color Setting
 library(RColorBrewer)
 #brewer.pal.info
+#terrain.colors(17)
 # Color for GDP Distribution Map
 Color_Palette_Assets <- colorRampPalette(brewer.pal(9,"Reds"))
 Color_Palette_Rivers <- colorRampPalette(c("deepskyblue"))
 Color_Palette_MainRv <- colorRampPalette(c("blue"))
 Color_Palette_Impact <- colorRampPalette(c(rgb(1,1,0,0.2)), alpha = TRUE)
-Color_Palette_Height <- 
-  colorRampPalette(
-    c(
-      "darkolivegreen3",
-      "palegreen4",
-      "cornsilk",
-      "cornsilk2",
-      "cornsilk3",
-      "gray80",
-      "gray70",
-      "gray60",
-      "gray50",
-      "gray40",
-      "snow3",
-      "snow2",
-      "snow1",
-      "snow"
-    )
-  )
+Color_Palette_Height <- colorRampPalette(c("#74CA00FF", 
+                                           "#97D300FF", "#BDDC00FF",
+                                           "#E6E600FF", "#E7D01AFF",
+                                           "#E8C034FF", "#EAB64EFF",
+                                           "#EBB169FF", "#EDB283FF",
+                                           "#EEB99FFF", "#EFC6BAFF",
+                                           "#F1D9D6FF", "#F2F2F2FF"))
+
 
 
 # China Yangtze Flooding
@@ -55,14 +52,15 @@ SP_Rivers_Country <- intersect(SP_Rivers, SP_Country)
 
 # Yangtze River Area Flooding
 # https://www.reuters.com/article/us-china-weather-floods/red-alerts-in-china-as-floods-maroon-equipment-to-fight-coronavirus-idUSKCN24I0G0
-SP_Rivers_Yangtze <- SP_Rivers_Country[SP_Rivers_Country$name.1 %in% c("Yangtze","Chang Jiang"),]
-SP_Impact <- buffer(SP_Rivers_Yangtze, width=DP_Margin/100)
+SP_Rivers_Flooded <- SP_Rivers_Country[SP_Rivers_Country$name.1 %in% c("Yangtze","Chang Jiang"),]
+SP_Impact <- buffer(SP_Rivers_Flooded, width=DP_Margin/100)
 
+# 
+source("Ref.R")
 # Altitude Map
 Raster_Alt_Country <- getData('alt', country=DP_Country_ISO3, mask=TRUE, path = Ref.DIR_DATA_Ref)
 
 # GDP Distribution Map
-source("Ref.R")
 source(Ref.DIR_FUNC_LitPop)
 Raster_GDP_Proxy_Country <- LitPop.Get_GDP_Proxy_Raster(DP_Country_ISO3, DP_Year)
 source(Ref.DIR_FUNC_MEF)
@@ -71,6 +69,16 @@ Raster_GDP_Country <- Raster_GDP_Proxy_Country * (DF_Country_GDP$value / 10 ^ 6)
 
 
 
+if(DP_Save)
+{
+  png(
+    width = DP_Width,
+    height = DP_Height,
+    pointsize = DP_Pointsize,
+    bg = "transparent",
+    file = paste0(Ref.DIR_Outputs, SP_Country$name, " Elevation.png")
+  )
+}
 #A. Plot Country Contour
 ########################
 plot(Raster_Alt_Country,  col=Color_Palette_Height(255))
@@ -78,17 +86,31 @@ plot(Raster_Alt_Country,  col=Color_Palette_Height(255))
 plot(SP_States, add=T)
 # Plot Rivers
 plot(SP_Rivers_Country, add=T, lwd=SP_Rivers_Country$strokelwd/5, col=Color_Palette_Rivers(1))
-plot(SP_Rivers_Yangtze, add=T, lwd=SP_Rivers_Yangtze$strokelwd/5, col=Color_Palette_MainRv(1))
+plot(SP_Rivers_Flooded, add=T, lwd=2, col=Color_Palette_MainRv(1))
 # Label State Names
 # MX_Coordinates <- coordinates(SP_States)
 # text(x=MX_Coordinates[,1],
 #      y=MX_Coordinates[,2],
 #      SP_States$name,
-#      cex = 0.55)
+#      cex = 1.0)
 ########################
+if(DP_Save)
+{
+  dev.off()
+}
 
 
 
+if(DP_Save)
+{
+  png(
+    width = DP_Width,
+    height = DP_Height,
+    pointsize = DP_Pointsize,
+    bg = "transparent",
+    file = paste0(Ref.DIR_Outputs, SP_Country$name, " GDP Distribution.png")
+  )
+}
 #B. Plot GDP Density Map
 ########################
 plot(Raster_GDP_Country[SP_States_Core, drop=F], col=Color_Palette_Assets(100))
@@ -96,7 +118,7 @@ plot(Raster_GDP_Country[SP_States_Core, drop=F], col=Color_Palette_Assets(100))
 plot(SP_States_Core, add=T)
 # Plot Rivers
 plot(SP_Rivers_Country, add=T, lwd=SP_Rivers_Country$strokelwd/5, col=Color_Palette_Rivers(1))
-plot(SP_Rivers_Yangtze, add=T, lwd=SP_Rivers_Yangtze$strokelwd/5, col=Color_Palette_MainRv(1))
+plot(SP_Rivers_Flooded, add=T, lwd=2, col=Color_Palette_MainRv(1))
 # Label State Names
 MX_Coordinates <- coordinates(SP_States_Core)
 text(x=MX_Coordinates[,1],
@@ -106,6 +128,10 @@ text(x=MX_Coordinates[,1],
 # Plot Impacted Area
 plot(SP_Impact, add=T, col=Color_Palette_Impact(1))
 ########################
+if(DP_Save)
+{
+  dev.off()
+}
 
 
 
